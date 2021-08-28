9479150640fbcf44da1a8a660a9938a2a58962ca
rudrashan16 <74367420+rudrashan16@users.noreply.github.com>
2021-08-28 02:37:59 -0700

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,26 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("B U G G U O Y E", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+
+    
+    fill("white")
+    textSize(15)
+    text("T U N A K T U N T U N ", 700,150)
+    fill("lightblue")
+    text("Hint: Performs a particular task", 700,170)
+
+
+    fill("white")
+    textSize(15)
+    text("T U R U R A R A", 100,250)
+    fill("lightblue")
+    text("Hint: Stores all information", 100,270)
+}
